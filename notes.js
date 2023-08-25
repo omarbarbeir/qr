76c320aae8c5f18cdf00fdbc2be4192ef1afb0a8
omarbarbeir <138704845+omarbarbeir@users.noreply.github.com>
2023-08-26 00:10:29 +0300

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,12 @@
+let imgbox = document.getElementById("imgbox")
+let qrimg = document.getElementById("qrimg")
+let qrtext = document.getElementById("qrtext")
+
+
+
+function generateQR(){
+    if(qrtext.value.length >0){
+        qrimg.src= "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+        imgbox.classList.add("show")
+    }
+}
